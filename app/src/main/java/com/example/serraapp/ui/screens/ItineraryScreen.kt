da901b274270d60330aef263c456a1e3ecb9927c
Ariane Silva <arianeeng2023@gmail.com>
2026-06-24 09:37:57 -0300

    update: incrementando caixa de texto e tela em roteiros

--- app/src/main/java/com/example/serraapp/ui/screens/ItineraryScreen.kt
+++ app/src/main/java/com/example/serraapp/ui/screens/ItineraryScreen.kt
@@ -1,33 +1,50 @@
 package com.example.serraapp.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.mutableStateOf
 import com.example.serraapp.model.TouristPlace
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
 
 @Composable
 fun ItineraryScreen(
     onPlaceClick: (TouristPlace) -> Unit
 ){
-    Scaffold() { paddingValues ->
+    var nameItinerary by remember {
+        mutableStateOf("")
+    }
+    Scaffold { paddingValues ->
         LazyColumn(
             modifier = Modifier
-                .padding(paddingValues),
-            verticalArrangement = Arrangement.Center,
+                .padding(paddingValues)
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             item {
                 Text(
-                    text = "Seus roteiros",
+                    text = "Crie seu Roteiro",
                     style = MaterialTheme.typography.bodyLarge
+                )
+            }
+            item {
+                TextField(
+                    value = nameItinerary,
+                    onValueChange = {
+                        nameItinerary = it
+                    },
+                    label = {
+                        Text("Nome do Roteiro")
+                    }
                 )
             }
         }
