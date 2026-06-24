259cff1f5bd332e352c74c9f327b143d01120e93
Ariane Silva <arianeeng2023@gmail.com>
2026-06-24 18:24:54 -0300

    update: ajustando topBar conScaffold

--- app/src/main/java/com/example/serraapp/ui/components/SerraTopBar.kt
+++ app/src/main/java/com/example/serraapp/ui/components/SerraTopBar.kt
@@ -10,41 +10,48 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.WbCloudy
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SerraTopBar(){
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-
-        horizontalArrangement = Arrangement.SpaceBetween
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(10.dp)
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.WbCloudy,
+                                contentDescription = "Clima"
+                            )
+                            Text(
+                                text = "SerraApp",
+                            )
+                        }
+                },
+                actions = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Pesquisar"
+                        )
+                    }
+                }
+            )
+        },
     ) {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Icon(
-                imageVector = Icons.Default.WbCloudy,
-                contentDescription = "Clima"
-            )
-            Text(
-                text = "SerraApp",
-                style = MaterialTheme.typography.titleLarge
-            )
-        }
-        IconButton(
-            onClick = {}
-        ) {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = "Pesquisar"
-            )
-        }
+
     }
 }
