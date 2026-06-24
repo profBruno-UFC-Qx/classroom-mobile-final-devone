af805d726fca9a605d4ed948db5cf6856a46ad75
Ariane Silva <arianeeng2023@gmail.com>
2026-06-24 17:19:14 -0300

    update: ajustando tela de favoritos, qaunto a posicionamento

--- app/src/main/java/com/example/serraapp/ui/screens/FavoritesScreen.kt
+++ app/src/main/java/com/example/serraapp/ui/screens/FavoritesScreen.kt
@@ -1,8 +1,5 @@
 package com.example.serraapp.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -10,14 +7,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.serraapp.data.places
 import com.example.serraapp.model.TouristPlace
-import com.example.serraapp.ui.components.BottomBar
 import com.example.serraapp.ui.components.PlaceCard
-import com.example.serraapp.ui.components.SerraTopBar
 
 @Composable
 fun FavoritesScreen(
@@ -32,7 +26,11 @@ fun FavoritesScreen(
                 .padding(16.dp)
         ) {
             item {
-                Text("Seus favoritos")
+                Text(
+                    text = "Seus favoritos",
+                    style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
 
             }
             items(favoritePlaces){ place ->
