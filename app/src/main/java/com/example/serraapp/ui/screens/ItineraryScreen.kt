804d85e91a3fa654447360ba267bd80e49cea7b2
Ariane Silva <arianeeng2023@gmail.com>
2026-06-24 22:48:13 -0300

    update: checkbox de lugares para roteiro

--- app/src/main/java/com/example/serraapp/ui/screens/ItineraryScreen.kt
+++ app/src/main/java/com/example/serraapp/ui/screens/ItineraryScreen.kt
@@ -1,8 +1,18 @@
 package com.example.serraapp.ui.screens
 
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,6 +25,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import com.example.serraapp.data.places
 
 @Composable
 fun ItineraryScreen(
@@ -22,6 +34,9 @@ fun ItineraryScreen(
 ){
     var nameItinerary by remember {
         mutableStateOf("")
+    }
+    var checked by remember {
+        mutableStateOf(false)
     }
     Scaffold { paddingValues ->
         LazyColumn(
@@ -46,6 +61,45 @@ fun ItineraryScreen(
                         Text("Nome do Roteiro")
                     }
                 )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+            item {
+                Text(
+                    text = "Locais disponíveis",
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+            items(places) { place ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 4.dp, start = 4.dp, end = 4.dp)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(place.name)
+                        Checkbox(
+                            checked = checked,
+                            onCheckedChange = {
+                                checked = it
+                            }
+                        )
+                    }
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.height(24.dp))
+                Button(
+                    onClick = {},
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Salvar roteiro")
+                }
             }
         }
     }
